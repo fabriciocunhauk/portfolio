7df9df7aa078debd1a13e63e75dc47ff78b4bef9
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2021-01-29 04:22:42 +0000

    Added svgs data, changed menu color and created skill section

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { mySvgData } from '../../svg-data';
 import './about-section.scss';
 
 const AboutSection = () => {
     return (
-        <div className="about-container">
-            <h1>About</h1>
+        <div className="skill-container">
+            <h1>My Skills</h1>
             <hr />
-
+            <div className="skill-container-content">
+                <div className="skill-container-content-svg">
+                    {mySvgData.map(svgLink =>
+                        <a key={svgLink.id} href={svgLink.link} rel="noopener noreferrer" target="_blank"><img src={svgLink.path} alt="name" className="skill-container-content-svg-image" /></a>
+                    )}
+                </div>
+                <div className="skill-container-side-text">
+                    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, deserunt?</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dolorem architecto ipsam, voluptatum nulla quis blanditiis, aliquid saepe error suscipit laudantium consectetur libero optio, sit autem adipisci corrupti nobis possimus aut numquam facilis! Doloribus illo repudiandae ea architecto? Non quam perspiciatis omnis nihil! Pariatur consectetur debitis, id consequuntur ipsa excepturi!</p>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/svg-data.js
@@ -0,0 +1,63 @@
+import javascript from './images/SVG/javascript-4.svg';
+import html from './images/SVG/html5.svg';
+import css from './images/SVG/css3.svg';
+import node from './images/SVG/node-js-logo.svg';
+import react from './images/SVG/react-1.svg';
+import redux from './images/SVG/redux.svg';
+import git from './images/SVG/git.svg';
+import github from './images/SVG/github-octocat.svg';
+import mySql from './images/SVG/mysql.svg';
+import bootstrap from './images/SVG/bootstrap-4.svg';
+
+export const mySvgData = [
+    {
+        id: 1,
+        path: javascript,
+        link: "https://www.javascript.com/"
+    },
+    {
+        id: 2,
+        path: html,
+        link: "https://www.w3schools.com/html/"
+    },
+    {
+        id: 3,
+        path: css,
+        link: "https://www.w3schools.com/css/default.asp"
+    },
+    {
+        id: 4,
+        path: node,
+        link: "https://nodejs.org/en/"
+    },
+    {
+        id: 5,
+        path: react,
+        link: "https://reactjs.org/"
+    },
+    {
+        id: 6,
+        path: redux,
+        link: "https://redux.js.org/"
+    },
+    {
+        id: 7,
+        path: git,
+        link: "https://git-scm.com/"
+    },
+    {
+        id: 8,
+        path: github,
+        link: "https://github.com/"
+    },
+    {
+        id: 9,
+        path: mySql,
+        link: "https://www.mysql.com/"
+    },
+    {
+        id: 10,
+        path: bootstrap,
+        link: "https://getbootstrap.com/"
+    },
+]
