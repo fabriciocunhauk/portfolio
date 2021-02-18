79db1185cbb8e5fbc5b54a7a6e76dcd1f6ddcb76
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2021-02-18 03:55:52 +0000

    Main page header animation

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,20 +2,40 @@ import React from 'react';
 import mainBg from '../../images/pexels-neo-2653362.jpg';
 import AboutSection from '../../components/AboutSection/AboutSection';
 import ProjectsSection from '../../components/ProjectsSection/ProjectSection';
+import Typical from 'react-typical';
 
 import './main-page.scss';
 import ContactSection from '../../components/ContactSection/ContactSection';
 // import SideMenu from '../../components/SideMenu/SideMenu';
 
 const MainSection = () => {
+
+
     return (
         <>
             <img className="main-bg" src={mainBg} alt="main-background" />
             <div className="main-container" id="/">
                 {/* <SideMenu /> */}
                 <header>
-                    <h1>Hi, I'm Fabricio Cunha</h1>
-                    <p>A Frontend Developer</p>
+
+                    <h1>Hi,
+                    <Typical
+                            wrapper="b"
+                            steps={[
+                                1000,
+                                " I'm Fabricio Cunha",
+                            ]}
+                        />
+                    </h1>
+                    <p>
+                        <Typical
+                            wrapper="b"
+                            steps={[
+                                3000,
+                                "A Frontend Developer",
+                            ]}
+                        />
+                    </p>
                 </header>
             </div>
             <AboutSection />
