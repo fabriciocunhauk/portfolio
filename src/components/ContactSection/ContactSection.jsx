05f08144e103d1b318c0ac0085e5221a6fdc0739
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-03-07 03:29:34 +0000

    Added formspree and changed form functionality

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './contact-section.scss';
 
 const ContactSection = () => {
+    const [status, setStatus] = useState('');
+
+    console.log(status);
+
+    function submitForm(ev) {
+        console.log(ev);
+        ev.preventDefault();
+        const form = ev.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                form.reset();
+                setStatus("SUCCESS");
+            } else {
+                setStatus("ERROR");
+            }
+        };
+        xhr.send(data);
+    }
+
     return (
         <div className="contact-section-container" id="contact-section">
             <h1>Lets talk</h1>
             <hr />
-            <form action="mailto:fabriciocunhadeveloper@gmail.com" method="post" encType="text/plain">
-                <fieldset>
-                    <input id="name" type="text" name="Name " placeholder="Name" required />
-                    <input type="email" id="email" name="email " placeholder="Enter email" required />
-                    <input type="tel" id="contact" name="phone " placeholder="Contact number" required />
+            <form action="https://formspree.io/f/mpzogvej" method="POST" onSubmit={submitForm}>
+                <fieldset name="portfolio-form">
+                    <input id="name" type="text" name="Name" placeholder="Name" required />
+                    <input type="email" id="email" name="email" placeholder="Enter email" required />
+                    <input type="tel" id="contact" name="phone" placeholder="Contact number" required />
                 </fieldset>
-                <textarea name="aditional-details" cols="64" rows="10" placeholder="Your Message" required></textarea>
-                <button type="submit">Send Message</button>
+                <textarea name="additional-details" cols="64" rows="10" placeholder="Your Message" required></textarea>
+                {
+                    status === "SUCCESS"
+                        ? <button className="btn-success">Thanks!</button>
+                        : status === "ERROR"
+                            ? <button className="btn-fail">Ooops! There was an error.</button>
+                            : <button>Submit</button>
+                }
             </form>
         </div>
     )
