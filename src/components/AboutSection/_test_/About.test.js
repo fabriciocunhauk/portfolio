aae39b8017d796e3cd61e3be6a21e76db9271eaa
fabriciocunhauk <fabriciocunhadeveloper@gmail.com>
2021-11-03 15:16:48 +0000

    Added tests to about section

---
+++ src/components/AboutSection/_test_/About.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import AboutSection from '../AboutSection'
+
+describe('AboutSection', () => {
+    test('should render about component', () => {
+        const text = "Skills"
+        const text2 = "JavaScript | Typescript | HTML | CSS | SCSS/SASS | React.js | Next.js | Node.js | Redux | SQL | NoSQL | REST Api's | Git"
+        render(<AboutSection />)
+        screen.getByText(text)
+        // fails
+        // expect(screen.getByText(text + 'About')).toBeInTheDocument()
+ 
+        // succeeds
+        expect(screen.getByText(text)).toBeInTheDocument();
+        expect(screen.getByText(text2)).toBeInTheDocument();
+    
+        // succeeds
+        expect(screen.getByText(/Skills/)).toBeInTheDocument()
+    })
+})
