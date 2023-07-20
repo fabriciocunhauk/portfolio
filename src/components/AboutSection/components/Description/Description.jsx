d4d61fd6d6f17e912e826427846e4faae3de9b5d
fabriciocunhauk <fabriciocunhadeveloper@gmail.com>
2023-07-20 12:39:48 +0100

    Fixed responsiveness to the skills section

--- src/components/AboutSection/components/Description/Description.jsx
+++ src/components/AboutSection/components/Description/Description.jsx
@@ -1,11 +1,10 @@
 const Description = ({skillAnimation}) => {
 return (
         <div className={skillAnimation ? "skill-container-side-text skill-container-side-text-animation" : "skill-container-side-text"}>
-                    <h1 className="skill-container-side-text-h1">
-                        Hey, Im Fabricio Cunha</h1>
-                        <h2>a self-taught front-end developer.</h2>
-                    <p>Hi there! I'm driven and passionate software engineer with a strong track record of delivering outstanding results. I have 3 years of experience in software development, with expertise such as frontend development with JavaScript, Typescript, HTML5, CSS3, React.js, REMIX JS, Tailwind, Sass, Styled-components, MUI, Redux, GraphQL, APIs, Git And GitHub.</p>
-                </div>
+            <h2>Hey, Im Fabricio Cunha</h2>
+            <h3>A self-taught Front-end Developer.</h3>
+            <p>Hi there! I'm driven and passionate software engineer with a strong track record of delivering outstanding results. I have 3 years of experience in software development, with expertise in Frontend Development such as  JavaScript, Typescript, HTML5, CSS3, React.js, REMIX JS, Tailwind, Sass, Styled-components, MUI, Redux, GraphQL, APIs, Git And GitHub.</p>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './index.scss';
+
+createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+      <App />
+  </React.StrictMode>
+  );
